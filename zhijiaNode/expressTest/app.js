6ad2b25f2ad3ac4c628c1170dabd28b48802df01
javascriptchen <458679677@qq.com>
2018-04-12 23:32:09 +0800

    express启动

--- zhijiaNode/expressTest/app.js
+++ zhijiaNode/expressTest/app.js
@@ -2,11 +2,18 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  console.log(req.query);
-  
-  res.send('Hello World');
+  // console.log(req.query);
+  // req.params.id
+  // res.send();
+  // res.json({
+  //   id:req.params.id
+  // })
+  res.render("")
 })
 
 var server = app.listen(8081, function () {
   console.log("借口已启动")
 })
+// 1.安装并引用express 启动一个express实例；
+// 2.app listen一个端口，启动一个后台服务
+// 3.app.get 设置基础的路由，然后吐出数据
