22ef9f2357c9cff6d2ab1fbc12f39f10bb763a58
javascriptchen <458679677@qq.com>
2018-04-13 00:33:41 +0800

    express基础使用

--- zhijiaNode/expressTest/app.js
+++ zhijiaNode/expressTest/app.js
@@ -1,18 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+})
 app.use(express.static('public'));
-app.get('/', function (req, res) {
-  // console.log(req.query);
-  // req.params.id
-  // res.send();
-  // res.json({
-  //   id:req.params.id
-  // })
-  res.render("")
+
+app.get('/index', function (req, res) {
+  res.sendFile(__dirname + "/views/" + "index.html")
+})
+app.post('/index', urlencodedParser, function (req, res) {
+  res.redirect("https://www.baidu.com/s?tn=99006304_6_oem_dg&isource=infinity&wd="+req.body.data+"")
 })
 
 var server = app.listen(8081, function () {
-  console.log("借口已启动")
+  console.log("接口已启动")
 })
 // 1.安装并引用express 启动一个express实例；
 // 2.app listen一个端口，启动一个后台服务
