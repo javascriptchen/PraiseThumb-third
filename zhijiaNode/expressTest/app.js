7b9999f157c4e6d73f292aa7572f62b2dd1a12b5
javascriptchen <458679677@qq.com>
2018-04-12 23:57:38 +0800

    app.use

--- zhijiaNode/expressTest/app.js
+++ zhijiaNode/expressTest/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+app.use(express.static('public'));
 app.get('/', function (req, res) {
   // console.log(req.query);
   // req.params.id
@@ -17,3 +17,6 @@ var server = app.listen(8081, function () {
 // 1.安装并引用express 启动一个express实例；
 // 2.app listen一个端口，启动一个后台服务
 // 3.app.get 设置基础的路由，然后吐出数据
+// 4.平时的请求都是get，浏览器直接敲
+// 5.get post put delete 
+// req.query req.params res.send() res.json()
