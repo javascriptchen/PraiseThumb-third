4870c785b8cfa617e1effdb3f49650c852fb0802
javascriptchen <458679677@qq.com>
2018-04-29 21:49:43 +0800

    错误处理

--- node/zhijiaNode/expressTest/app.js
+++ node/zhijiaNode/expressTest/app.js
@@ -9,16 +9,28 @@ app.use(function (req, res, next) {
   console.log("必经路由");
   next();
 })
-app.get('/index', function (req, res, next) {
-  req.data = 123;
-  next();
-}, function (req, res, next) {
-  console.log(req.data);
-  res.send("123");
-});
-app.get('/go', function (req, res, next) {
-  res.send('go')
-});
+
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
+
+function logErrors(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
+
+function clientErrorHandler(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ error: 'Something blew up!' });
+  } else {
+    next(err);
+  }
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.end(err);
+}
 
 var server = app.listen(8081, function () {
   console.log("接口已启动")
