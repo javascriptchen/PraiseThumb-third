deb76dde23d181c40ee3df874323fbcf574fa86e
javascriptchen <458679677@qq.com>
2018-04-30 13:58:52 +0800

    结束expressDemo

--- node/zhijiaNode/expressTest/app.js
+++ node/zhijiaNode/expressTest/app.js
@@ -1,36 +1,16 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
+swig = require('swig'),
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
 
-// app.get('/*', function (req, res, next) {
-//   console.log("必经路由");
-//   next();
-// });
-app.use(function (req, res, next) {
-  console.log("必经路由");
-  next();
-})
-
-app.use(logErrors);
-app.use(clientErrorHandler);
-app.use(errorHandler);
-
-function logErrors(err, req, res, next) {
-  console.error(err.stack);
-  next(err);
-}
-
-function clientErrorHandler(err, req, res, next) {
-  if (req.xhr) {
-    res.status(500).send({ error: 'Something blew up!' });
-  } else {
-    next(err);
-  }
-}
-
-function errorHandler(err, req, res, next) {
-  res.status(500);
-  res.end(err);
-}
+app.get('/', function (req, res, next) {
+  res.render('index',{
+    title:"测试首页",
+    data:'index'
+  })
+});
 
 var server = app.listen(8081, function () {
   console.log("接口已启动")
