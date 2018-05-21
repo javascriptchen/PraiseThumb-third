bd84bc3b6b568cc08d23d515fc1add526c11a67d
javascriptchen <458679677@qq.com>
2018-05-21 16:57:33 +0800

    数据结构数组

---
+++ demo/java/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        for (int i = 0;i < arr.length; i++){
+            arr[i] = i;
+        }
+
+        int[] score = new int[]{100,99,2};
+        for(int i = 0;i<score.length;i++)
+        {
+            System.out.println(score[i]);
+        }
+    }
+}
--- demo/python/pachong/spider.py
+++ demo/python/pachong/spider.py
@@ -1,9 +1,10 @@
 from urllib import request
 import re
 class Spider():
-    url = "https://www.zhihu.com/#signin"
-    root_pattern = '<div class="shizhan-course-wrap l ">[\s\S]*?</div>'
-
+    url = "https://www.panda.tv/cate/lol?pdt=1.24.s1.3.5rk1mo4tj4c"
+    root_pattern = '<div class="video-info">([\s\S]*?)</div>'
+    name_pattern  = '</i>([\s\S]*?)</span>'
+    number_pattern  = '<span class="video-number">([\s\S]*?)</span>'
 
     def __fetch_content(self):
         r = request.urlopen(Spider.url)
@@ -12,14 +13,43 @@ class Spider():
         return htmls
     
     def __analysis(self,htmls):
-
         root_html = re.findall(Spider.root_pattern,htmls)
+        anchors = []
+        for html in root_html:
+            name = re.findall(Spider.name_pattern,html)
+            number = re.findall(Spider.number_pattern,html)
+            anchor = {'name':name,'number':number}
+            anchors.append(anchor)
+        return anchors
 
-        a=1
+    def __refine(self,anchors):
+        l = lambda anchor:{
+            'name':anchor['name'][0].strip(),
+            'number':anchor['number'][0]
+        }
+        return map(l,anchors)
+
+    def __sort(self,anchors):
+        anchors = sorted(anchors, key=self.__sort_seet, reverse=True)# key写排序函数的逻辑
+        return anchors
+
+    def __sort_seet(self,anchor):
+        r = re.findall('\d*',anchor['number'])
+        number = float(r[0])
+        if('万' in anchor['number']):
+            number *= 10000
+        return number
+
+    def __show(self,anchors):
+        for rank in range(len(anchors)):
+            print('rank '+str(rank+1)+': '+anchors[rank]['name']+'-----'+anchors[rank]['number'])
 
     def go(self):
         htmls = self.__fetch_content()
-        self.__analysis(htmls)
+        anchors = self.__analysis(htmls)
+        anchors = list(self.__refine(anchors))
+        anchors = self.__sort(anchors)
+        self.__show(anchors)
 
 spider = Spider()
 spider.go()
