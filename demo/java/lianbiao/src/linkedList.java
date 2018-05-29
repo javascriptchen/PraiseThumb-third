3e1f571365b21eb89c92660eb98bf3c5c3443aeb
javascriptchen <458679677@qq.com>
2018-05-29 16:35:44 +0800

    链表

---
+++ demo/java/lianbiao/src/linkedList.java
@@ -0,0 +1,73 @@
+import java.util.LinkedList;
+
+/**
+ * Created by Administrator on 2018/5/29.
+ */
+public class linkedList<E> {
+    private class Node{
+        public E e;
+        public Node next;
+
+        public Node(E e , Node next){
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e){
+            this(e,null);
+        }
+
+        public Node(){
+            this(null,null);
+        }
+
+        @Override
+        public String toString(){
+            return e.toString();
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public LinkedList(){
+        head = null;
+        size = 0;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void addFirst(E e){
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+        head = new Node(e, head);
+        size++;
+    }
+
+    public void add(int index, E e){
+        if(index < 0 || index > size){
+            throw new IllegalArgumentException("err");
+        }
+
+        if(index == 0){
+            addFirst(e);
+        }
+        else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++){
+                prev = prev.next;
+            }
+
+            Node node = new Node(e);
+            node.next = prev.next;
+            prev.next = node;
+        }
+    }
+}
