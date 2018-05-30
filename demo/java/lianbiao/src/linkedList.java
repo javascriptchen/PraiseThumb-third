bbe89369ed55922a664086e5aff52297d84d9b2c
javascriptchen <458679677@qq.com>
2018-05-30 10:03:01 +0800

    链表

--- demo/java/lianbiao/src/linkedList.java
+++ demo/java/lianbiao/src/linkedList.java
@@ -1,9 +1,9 @@
-import java.util.LinkedList;
+
 
 /**
  * Created by Administrator on 2018/5/29.
  */
-public class linkedList<E> {
+public class LinkedList<E> {
     private class Node{
         public E e;
         public Node next;
@@ -27,11 +27,11 @@ public class linkedList<E> {
         }
     }
 
-    private Node head;
+    private Node dummyHead;
     private int size;
 
     public LinkedList(){
-        head = null;
+        dummyHead = new Node(null,null);
         size = 0;
     }
 
@@ -43,31 +43,33 @@ public class linkedList<E> {
         return size == 0;
     }
 
-    public void addFirst(E e){
-//        Node node = new Node(e);
-//        node.next = head;
-//        head = node;
-        head = new Node(e, head);
-        size++;
-    }
+
 
     public void add(int index, E e){
         if(index < 0 || index > size){
             throw new IllegalArgumentException("err");
         }
 
-        if(index == 0){
-            addFirst(e);
-        }
-        else {
-            Node prev = head;
-            for (int i = 0; i < index - 1; i++){
-                prev = prev.next;
-            }
 
-            Node node = new Node(e);
-            node.next = prev.next;
-            prev.next = node;
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++){
+            prev = prev.next;
         }
+
+//            Node node = new Node(e);
+//            node.next = prev.next;
+//            prev.next = node;
+        prev.next = new Node(e,prev.next);
+
+        size++;
+
+    }
+
+    public void addLast(E e){
+        add(size, e);
+    }
+
+    public void addFirst(E e){
+        add(0, e);
     }
 }
