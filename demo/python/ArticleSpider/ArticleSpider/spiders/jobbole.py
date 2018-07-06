9db3c345ff31a6678ed129e3721d2541e27b4253
javascriptchen <458679677@qq.com>
2018-07-06 17:47:50 +0800

    python黑马

--- demo/python/ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ demo/python/ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -19,19 +19,19 @@ class JobboleSpider(scrapy.Spider):
         1. 获取文章列表页中的文章url并交给scrapy下载后并进行解析
         2. 获取下一页的url并交给scrapy进行下载， 下载完成后交给parse
         """
+
         # 解析列表页中的所有文章url并交给scrapy下载后并进行解析
         post_nodes = response.css("#archive .floated-thumb .post-thumb a")
         for post_node in post_nodes:
             image_url = post_node.css("img::attr(src)").extract_first("")
             post_url = post_node.css("::attr(href)").extract_first("")
-            yield Request(url=parse.urljoin(response.url, post_url),
-                          meta={"front_image_url": image_url},
-                          callback=self.parse_detail)
+            yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url": image_url}, callback=self.parse_detail)
 
         # 提取下一页并交给scrapy进行下载
-        next_urls = response.css(".next.page-numbers::attr(href)").extract_first("")
-        if next_urls:
-            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse)
+        next_url = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_url:
+            yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse, dont_filter=True)
+
 
     def parse_detail(self, response):
         article_item = JobBoleArticleItem()
