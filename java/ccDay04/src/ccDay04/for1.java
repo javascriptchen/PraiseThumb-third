e572b48e876f92b036fa505d4e1d6e9bd7953e31
chenchen <458679677@qq.com>
2018-08-07 16:12:40 +0800

    java 水仙花数

--- java/ccDay04/src/ccDay04/for1.java
+++ java/ccDay04/src/ccDay04/for1.java
@@ -4,14 +4,13 @@ public class for1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int sum = 0;
-		for(int i = 0; i <= 100; i++) {
-			if(i%2 != 0) {
-				sum +=i;
+		for(int i = 100; i <= 999; i++) {
+			int bai = i/100;
+			int shi = i%100/10;
+			int ge = i%10;
+			if(i == bai*bai*bai+shi*shi*shi+ge*ge*ge) {
+				System.out.println(i);
 			}
-			
 		}
-		System.out.println(sum);
 	}
-
 }
