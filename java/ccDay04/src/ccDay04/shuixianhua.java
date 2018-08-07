f87d7d0f86b8086231638f3b4ed0d94f7d1e52cd
chenchen <458679677@qq.com>
2018-08-07 16:14:30 +0800

    java 水仙花数的个数

--- java/ccDay04/src/ccDay04/shuixianhua.java
+++ java/ccDay04/src/ccDay04/shuixianhua.java
@@ -4,14 +4,18 @@ public class shuixianhua {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int  num = 0;
+		
 		for(int i = 100; i <= 999; i++) {
 			int bai = i/100;
 			int shi = i%100/10;
 			int ge = i%10;
 			if(i == bai*bai*bai+shi*shi*shi+ge*ge*ge) {
 				System.out.println(i);
+				num++;
 			}
 		}
+		System.out.println("总共有"+num+"个水仙花树");
 	}
 
 }
