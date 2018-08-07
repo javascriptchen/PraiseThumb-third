028a9c18bcc1a4e3cb64593438cfe679296d8e12
chenchen <458679677@qq.com>
2018-08-07 16:07:13 +0800

    java for循环练习

--- java/ccDay04/src/ccDay04/for1.java
+++ java/ccDay04/src/ccDay04/for1.java
@@ -5,9 +5,10 @@ public class for1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int sum = 0;
-		for(int i = 1; i <= 10; i++) {
-			
-			sum +=i;
+		for(int i = 0; i <= 100; i++) {
+			if(i%2 != 0) {
+				sum +=i;
+			}
 			
 		}
 		System.out.println(sum);
