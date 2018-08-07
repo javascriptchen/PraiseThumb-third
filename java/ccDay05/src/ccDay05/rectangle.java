3a057a49f331f4e80cc81dba2057f54bf047a129
chenchen <458679677@qq.com>
2018-08-07 16:39:37 +0800

    java99乘法表

--- java/ccDay05/src/ccDay05/rectangle.java
+++ java/ccDay05/src/ccDay05/rectangle.java
@@ -5,8 +5,8 @@ public class rectangle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int row = 4;
-		int column = 15;
-		for(int i = 1; i <= row; i++) {
+		int column = 5;
+		for(int i = row; i > 0; i--) {
 			for(int j = 1; j <= i; j++) {
 				System.out.print("* ");
 				
