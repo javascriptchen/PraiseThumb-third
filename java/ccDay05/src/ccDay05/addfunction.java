83a4ebbe39217ca603394916bb889bf9eb207456
chenchen <458679677@qq.com>
2018-08-07 17:47:35 +0800

    java方法比较大小

---
+++ java/ccDay05/src/ccDay05/addfunction.java
@@ -0,0 +1,24 @@
+package ccDay05;
+
+import java.util.Scanner;
+
+public class addfunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("请输入第一个数字");
+		int a = scanner.nextInt();
+		System.out.println("请输入第二个数字");
+		int b = scanner.nextInt();
+		
+		int max = getMax(a,b);
+		System.out.println("最大值为："+max);
+	}
+	
+	public static int getMax(int a, int b) {
+		int max = a > b ? a : b;
+		return max;
+	}
+
+}
