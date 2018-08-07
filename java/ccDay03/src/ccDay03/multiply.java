fa5125fdca517668fad04d7ecee818d6077ab64a
chenchen <458679677@qq.com>
2018-08-07 11:26:39 +0800

    java键盘录入

---
+++ java/ccDay03/src/ccDay03/multiply.java
@@ -0,0 +1,11 @@
+package ccDay03;
+
+public class multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(2 * 8);
+		System.out.println(2 << 3);
+	}
+
+}
