d93b299b3a165f8bb1d8978651e8ee0a76bfbbb5
chenchen <458679677@qq.com>
2018-08-08 10:51:51 +0800

    java数组练习

---
+++ java/ccDay06/src/ccDay06/week.java
@@ -0,0 +1,17 @@
+package ccDay06;
+
+import java.util.Scanner;
+
+public class week {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("请输入1-7的数字：");
+		char[] weeks = {'一','二','三','四','五','六','日'};
+		Scanner scanner = new Scanner(System.in);
+		int week = scanner.nextInt();
+		System.out.println("星期" + weeks[week-1]);
+		
+	}
+
+}
