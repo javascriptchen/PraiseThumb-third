3afe13bb3853d33f07f6cff4d6b1a67def898957
chenchen <458679677@qq.com>
2018-08-10 11:23:49 +0800

    java实现随机数

---
+++ java/CCDay10/src/round.java
@@ -0,0 +1,17 @@
+
+public class round {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		while (true) {
+			a = (int) (Math.random() * 100);
+			if (a != 0) {
+				break;
+			} 
+
+		}
+		System.out.println(a);
+	}
+
+}
