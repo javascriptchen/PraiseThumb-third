d28e5aa4c63638405800bd67a06f52cae57a0448
chenchen <458679677@qq.com>
2018-08-13 13:53:08 +0800

    调用java内部类

--- java/ccDay11/src/neibu/neibu.java
+++ java/ccDay11/src/neibu/neibu.java
@@ -3,17 +3,24 @@ package neibu;
 public class neibu {
 
 	public static void main(String[] args) {
-		Outer.Inner oi = new Outer().new Inner();
-		oi.print();
+//		Outer.Inner oi = new Outer().new Inner();
+//		oi.print();
+		Outer outer = new Outer();
+		outer.method();
 	}
 
 }
 
 class Outer{
 	private int num = 10;
-	class Inner{
+	private class Inner{
 		public void print() {
 			System.out.println(num);
 		}
 	}
+	public void method() {
+		Inner inner = new Inner();
+		inner.print();
+	}
+	
 }
