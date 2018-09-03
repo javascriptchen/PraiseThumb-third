c602a075f358fef4360f038df32e2ce17cd892b1
chenchen <458679677@qq.com>
2018-09-03 16:36:22 +0800

    io追加

--- java/ccDay20/src/com/heima/stream/Demo2_FileOutputStream.java
+++ java/ccDay20/src/com/heima/stream/Demo2_FileOutputStream.java
@@ -1,13 +1,12 @@
 package com.heima.stream;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Demo2_FileOutputStream {
 
 	public static void main(String[] args) throws IOException {
-		FileOutputStream fos = new FileOutputStream("yyy.txt");
+		FileOutputStream fos = new FileOutputStream("yyy.txt",true);
 		fos.write(97);
 		fos.write(98);
 		fos.close();
