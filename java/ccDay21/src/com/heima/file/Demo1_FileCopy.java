6bf362509222dd1cbc14f78ae12ce1c81c9c9336
chenchen <458679677@qq.com>
2018-09-04 23:07:11 +0800

    fileDir

--- java/ccDay21/src/com/heima/file/Demo1_FileCopy.java
+++ java/ccDay21/src/com/heima/file/Demo1_FileCopy.java
@@ -8,6 +8,20 @@ import java.io.IOException;
 public class Demo1_FileCopy {
 
 	public static void main(String[] args) throws IOException {
+		//demo1();
+		FileReader fr = new FileReader("aaa.txt");
+		FileWriter fw = new FileWriter("bbb.txt");
+		
+		char[] arr = new char[1024*8];
+		int len;
+		while((len = fr.read(arr)) != -1) {
+			fw.write(arr,0,len);
+		}
+		fr.close();
+		fw.close();
+	}
+
+	private static void demo1() throws FileNotFoundException, IOException {
 		FileReader fr = new FileReader("aaa.txt");
 		FileWriter fw = new FileWriter("bbb.txt");
 		int b;
---
+++ java/ccDay21/src/com/heima/file/Demo2_LineNumberReader.java
@@ -0,0 +1,9 @@
+package com.heima.file;
+
+public class Demo2_LineNumberReader {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
