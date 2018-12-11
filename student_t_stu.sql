ad3cf8ff08d5c3c6bccdb0a7b332527b2a26e0d7
javascriptchen <458679677@qq.com>
2018-12-11 22:44:47 +0800

    清空文件夹

--- student_t_stu.sql
+++
@@ -1,6 +0,0 @@
-UPDATE student.t_stu SET name = 'zhangsan', age = 23, chengji = 0 WHERE id = 1;
-UPDATE student.t_stu SET name = 'lisi', age = 38, chengji = 0 WHERE id = 2;
-UPDATE student.t_stu SET name = 'zhangsan', age = 25, chengji = 0 WHERE id = 3;
-UPDATE student.t_stu SET name = 'wanggang', age = 28, chengji = 0 WHERE id = 4;
-UPDATE student.t_stu SET name = 'ssdf', age = 28, chengji = 0 WHERE id = 6;
-UPDATE student.t_stu SET name = 'hehe', age = 23, chengji = 58 WHERE id = 10;
