70fae79f2d11af03b222fd5e46af3dcc7cbe818b
javascriptchen <458679677@qq.com>
2018-12-14 10:42:47 +0800

    webpack压缩public中的css，js并生成md5

---
+++ config/webpack.dev.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const webpack = require("webpack");
+const LiveReloadPlugin = require("webpack-livereload-plugin"); //监控浏览器自动更新
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: {
+		index: [
+			path.join(__dirname, "../src/public/script/index.es6"),
+			path.join(__dirname, "../src/public/script/index.js")
+		],
+		tag: [
+			path.join(__dirname, "../src/public/script/tag.es6")
+		]
+	},
+	output: {
+		filename: "public/scripts/[name]-[hash:5].js",
+		path: path.join(__dirname, "../build")
+	},
+	module: {
+		rules: [{
+			test: /\.es6$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: "babel-loader",
+				options: {
+					presets: ["env"]
+				}
+			}
+		},
+		{
+			test: /\.css$/,
+			use: ExtractTextPlugin.extract({
+				fallback: "style-loader",
+				use: "css-loader"
+			})
+		}
+		]
+	},
+	plugins: [
+		// 删除上次编译留下的文件，看着碍眼
+		new CleanWebpackPlugin(["build/public/**/*"], {
+			root: path.join(__dirname,"../"), //根目录
+			verbose: true, //开启在控制台输出信息
+			dry: false //启用删除文件
+		}),
+		// 打包公用js
+		new webpack.optimize.CommonsChunkPlugin({
+			name: "vendor",
+			filename: "public/scripts/common/vendor-[hash:5].js",
+		}),
+		new webpack.DefinePlugin({
+			"process.env": {
+				NODE_ENV: "dev"
+			}
+		}),
+		new LiveReloadPlugin({
+			appendScriptTag: true
+		}), //将<LiveReload>的script自动加入<head>中
+		new ExtractTextPlugin("public/css/[name]-[hash:5].css"),
+	]
+};
--- src/public/script/index.es6
+++ src/public/script/index.es6
@@ -1,49 +1,18 @@
+import css from "../css/index.css";
+
 class PraiseButton {
-	constructor(num, element) {
-		this.num = num;
-		this.element = element;
-	}
+	constructor() {}
 	clickAction() {
 		this.element.click(
-			this.throttle(() => {
-				if (this.num < 10) {
-					this.element.css("-webkit-filter", "grayscale(0)");
-					$("#animation").addClass("num");
-					this.num = window.add(this.num);
-					setTimeout(() => {
-						$("#animation").removeClass("num");
-					}, 1000);
-					axios.get("/index/update")
-						.then(res => {
+			axios.get("/index/update")
+				.then(res => {
 
-						})
-						.catch(err => {
+				})
+				.catch(err => {
 
-						});
-				} else {
-					this.element.css("-webkit-filter", "grayscale(1)");
-					this.num = 0;
-				}
-				console.log(this.num);
-			}, 500)
+				})
 		);
 	}
-
-	throttle(fn, wait) {
-		var timer;
-		return (...args) => {
-			timer = setTimeout(() => timer = null, wait);
-			return fn.apply(this, args);
-		};
-	}
 }
 
-class Thumb extends PraiseButton {
-	constructor(num, element) {
-		super(num, element);
-	}
-}
-
-export default {
-	Thumb
-};
+export default PraiseButton;
